8823a820a9d642988a3d20cb59ec3345b77f0d4b
Mario5999 <243716@ids.upchiapas.edu.mx>
2025-10-27 14:16:04 -0600

    develoment

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    spotify: {
+        CLIENT_ID: '4d0289c7a015430192a75c06703da263',
+        CLIENT_SECRET: '6269f584ffb84f269c4d8741023e0f6d',
+        AUTH_API_URL: 'https://www.google.com'
+    }
+};
